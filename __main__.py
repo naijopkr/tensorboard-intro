cadda5ba715a1ea8d7d9b84c6dc0002018e16e83
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-08-03 12:30:57 -0500

    implement model with tensorboard visualization

--- __main__.py
+++ __main__.py
@@ -1,2 +1,70 @@
-if __name__ == '__main__':
-    print('Hello world')
+import pandas as pd
+
+df = pd.read_csv('data/cancer_classification.csv')
+
+
+# Prepare data
+X = df.drop('benign_0__mal_1', axis=1)
+y = df['benign_0__mal_1']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.25,
+    random_state=101
+)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+# Create model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+
+early_stop = EarlyStopping(
+    monitor='val_loss',
+    mode='min',
+    verbose=1,
+    patience=25
+)
+
+log_dir = 'logs/fit'
+board = TensorBoard(
+    log_dir=log_dir,
+    histogram_freq=1,
+    write_graph=True,
+    write_images=True,
+    update_freq='epoch',
+    profile_batch=2,
+    embeddings_freq=1
+)
+
+model = Sequential()
+model.add(Dense(30, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(15, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam')
+
+
+# Training model
+model.fit(
+    X_train,
+    y_train,
+    epochs=600,
+    validation_data=(X_test, y_test),
+    verbose=1,
+    callbacks=[early_stop, board]
+)
+
+# Run `tensorboard --logdir logs/fit`
